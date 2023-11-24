7615d491fdf59881f7f9884fcfb33861d1c13ba3
Andra Urdea <urdeaandra22@stud.ase.ro>
2023-11-25 01:27:52 +0200

    generating an unique id based on the seat number and event id

--- ProjectPOO/Ticket.cpp
+++ ProjectPOO/Ticket.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include<string.h>
 
 
 using namespace std;
@@ -6,11 +8,31 @@ using namespace std;
 class Ticket {
 private:
 	char* ticketType;
-	int uniqueID;
+	int seatNumber;
+	char* eventID;
+	char* uniqueID;
 
-	Ticket() : ticketType(nullptr), uniqueID(0) {}
+public:
+	Ticket() : ticketType(nullptr), seatNumber(0), uniqueID(nullptr), eventID(nullptr) {}
 
-	Ticket(const char* ticketType, int uniqueID){}
+	Ticket(const char* ticketType, int uniqueID, const char* eventID){
+	
+	}
+
+	~Ticket() {
+		delete[] ticketType;
+		//delete[] seatNumber;
+		delete[] uniqueID;
+   }
+
+	void generateUniqueID() { //generating it based on the seat number and event id
+		std::string seatStr = std::to_string(seatNumber);
+		std::string eventIDStr(eventID);
+
+		uniqueID = new char[seatStr.length() + eventID.length() + 10];
+		strcpy(uniqueID, (seatStr + eventIDStr).c_str());
+		
+	}
 
 
 };
